4ce6b6f1d9ff6853107fb5fe3423be29b0b67653
Amanda Cadwell-Frost <acadwellfrost@gmail.com>
2026-04-12 10:00:08 -0400

    feat: wire public/private routing in App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import {
     Route,
     Redirect,
     Switch
-} from "react-router-dom"; 
+} from "react-router-dom";
 import { ThemeProvider } from '@material-ui/core/styles';
 import theme from './theme';
 import './css/style.css';
@@ -13,24 +13,29 @@ import NotFound from './NotFound';
 import FAQ from './FAQ';
 import Schedule from './Schedule';
 import Information from './Information';
+import PublicPage from './PublicPage';
+import Login from './Login';
+import ProtectedRoute from './Components/ProtectedRoute';
 
 function App() {
     return (
         <BrowserRouter>
             <ThemeProvider theme={theme}>
                 <Switch>
-                    <Route path="/" component={Information} exact />
-                    <Route path ="/schedule" component={Schedule} />
-                    <Route path ="/information" component={Information} />
-                    <Route path ="/faq" component={FAQ} />
+                    <Route path="/" component={PublicPage} exact />
+                    <Route path="/participants/login" component={Login} />
+                    <ProtectedRoute path="/participants/schedule" component={Schedule} />
+                    <ProtectedRoute path="/participants/information" component={Information} />
+                    <ProtectedRoute path="/participants/faq" component={FAQ} />
+                    <Redirect from="/schedule" to="/participants/schedule" />
+                    <Redirect from="/information" to="/participants/information" />
+                    <Redirect from="/faq" to="/participants/faq" />
                     <Route path="/404" component={NotFound} />
-                    <Redirect to='/404' />
+                    <Redirect to="/404" />
                 </Switch>
             </ThemeProvider>
         </BrowserRouter>
     );
 }
-
-
 
 export default hot(module)(App);
