8813e71e92be6367b06c036fbc9115617862fb61
Amanda Cadwell-Frost <acadwellfrost@gmail.com>
2026-04-12 09:56:50 -0400

    feat: add logout button to header

---
+++ src/Components/Header/Header.test.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import Header from './index';
+
+let mockPush;
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useHistory: () => ({ push: mockPush }),
+}));
+
+beforeEach(() => {
+  mockPush = jest.fn();
+  localStorage.setItem('harvestale_auth', 'true');
+});
+
+afterEach(() => {
+  localStorage.clear();
+});
+
+test('renders logo link', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <Header currentPage="Schedule" />
+    </MemoryRouter>
+  );
+  expect(getByText(/the harvest ale/i)).toBeTruthy();
+});
+
+test('logout button clears localStorage', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <Header currentPage="Schedule" />
+    </MemoryRouter>
+  );
+  fireEvent.click(getByText(/log out/i));
+  expect(localStorage.getItem('harvestale_auth')).toBeNull();
+});
+
+test('logout button redirects to /', () => {
+  const { getByText } = render(
+    <MemoryRouter>
+      <Header currentPage="Schedule" />
+    </MemoryRouter>
+  );
+  fireEvent.click(getByText(/log out/i));
+  expect(mockPush).toHaveBeenCalledWith('/');
+});
--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,14 +1,27 @@
 import React from "react";
-import { Link as RouterLink } from "react-router-dom";
+import { Link as RouterLink, useHistory } from "react-router-dom";
+import { Button } from "@mui/material";
 import ResponsiveNav from "./ResponsiveNav";
 
-const Header = (props) => (
-  <div className="header">
-    <ResponsiveNav currentPage={props.currentPage} />
-    <RouterLink to="/" className="header-logo logo-with-shadow">
-      The Harvest Ale
-    </RouterLink>
-    
-  </div>
-);
+const Header = (props) => {
+  const history = useHistory();
+
+  const handleLogout = () => {
+    localStorage.removeItem('harvestale_auth');
+    history.push('/');
+  };
+
+  return (
+    <div className="header">
+      <ResponsiveNav currentPage={props.currentPage} />
+      <RouterLink to="/" className="header-logo logo-with-shadow">
+        The Harvest Ale
+      </RouterLink>
+      <Button onClick={handleLogout} color="secondary" size="small">
+        Log out
+      </Button>
+    </div>
+  );
+};
+
 export default Header;
