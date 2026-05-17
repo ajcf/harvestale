4fcf37c7ac0e4a38f87484e5f7e32dbb595cc878
Amanda Cadwell-Frost <acadwellfrost@gmail.com>
2026-05-17 13:05:21 -0400

    style: schedule day header with amber name and fading rule

--- src/Components/ScheduleDay/index.js
+++ src/Components/ScheduleDay/index.js
@@ -1,28 +1,22 @@
-import Grid from '@mui/material/Grid';
 import ScheduleItem from '../ScheduleItem';
 
 const ScheduleDay = (props) => (
-    <Grid container className={`schedule-day`}>
-        <Grid container spacing={2} xs={12}>
-            <Grid item xs={8} className="schedule-day-label">
-                <div>{props.day}</div>
-            </Grid>
-            {/* <Grid item xs={6} className="schedule-date">
-                <div>{props.date}</div>
-            </Grid> */}
-        </Grid>
-        <Grid container className="schedule-day-items" spacing={2}>
-            {props.events.map((child) => (
-                <ScheduleItem
-                   key={child.label + "-" + child.time}
-                   label={child.label}
-                   time={child.time}
-                   location={child.location}
-                   description={child.description }
-                />
-            )) }
-        </Grid>
-    </Grid>
+  <div className="schedule-day">
+    <div className="schedule-day-header">
+      <div className="schedule-day-label">{props.day}</div>
+      <div className="schedule-day-rule" />
+    </div>
+    <div className="schedule-day-items">
+      {props.events.map((child) => (
+        <ScheduleItem
+          key={child.label + "-" + child.time}
+          label={child.label}
+          time={child.time}
+          description={child.description}
+        />
+      ))}
+    </div>
+  </div>
 );
 
 export default ScheduleDay;
