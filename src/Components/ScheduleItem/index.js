70d033e87f5061e7184e70e751576a71cd15a6a5
Amanda Cadwell-Frost <acadwellfrost@gmail.com>
2026-05-17 13:06:24 -0400

    style: schedule item clean flex rows, time right-aligned

--- src/Components/ScheduleItem/index.js
+++ src/Components/ScheduleItem/index.js
@@ -1,22 +1,13 @@
-
-import Grid from '@mui/material/Grid';
-
 const ScheduleItem = (props) => (
-    <Grid item className="schedule-item" xs={12}>
-        <Grid container spacing={2}>
-            <Grid item xs={6} className="schedule-time">
-                {props.time}
-            </Grid>
-            <Grid item xs={6} className="schedule-label">
-                {props.label}
-            </Grid>
-            {props.description &&
-                <Grid item xs={12} className="schedule-description">
-                    {props.description}
-                </Grid>
-            }
-        </Grid>
-    </Grid>
+  <div className="schedule-item">
+    <div className="schedule-item-row">
+      <div className="schedule-time">{props.time}</div>
+      <div className="schedule-label">{props.label}</div>
+    </div>
+    {props.description && (
+      <div className="schedule-description">{props.description}</div>
+    )}
+  </div>
 );
 
 export default ScheduleItem;
